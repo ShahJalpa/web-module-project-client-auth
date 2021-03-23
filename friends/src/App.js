0372e749aa13faae104a9ad9423092550781eae4
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-03-23 17:07:11 -0400

    login created, and working as expected, private route is also created.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Route, Link, Switch} from 'react-router-dom';
+
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Friends</h1>
+      <ul>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path='/login' component={Login} />
+      </Switch>
     </div>
   );
 }
